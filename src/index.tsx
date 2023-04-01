0977bbb51e4666d7ea6cbdadf1f1c8c783ba869c
Daniel Kaiser <danielkaiser@fastmail.com>
2023-04-01 22:07:09 +0200

    Upgrade the boilerplate files for React 18 with TS.

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,13 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import App from "./components/App";
-import { render } from "react-dom";
 
-render(<App />, document.getElementById("root"));
+import "./styles.css";
+import { createRoot } from "react-dom/client";
+
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const root = createRoot(document.getElementById("root")!);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
